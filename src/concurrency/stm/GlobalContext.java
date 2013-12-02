2079e9cd00ea0cae98fd4622734e7fd4fdcc35e4
Julia Valchuk <julia.valchuk@gmail.com>
2013-12-02 18:58:48 +0200

    Fair ClobalContext

--- src/concurrency/stm/GlobalContext.java
+++ src/concurrency/stm/GlobalContext.java
@@ -7,6 +7,8 @@ import java.util.Map;
  * @author mishadoff
  */
 public final class GlobalContext extends Context {
+    private HashMap<Ref, Object> inTxMap = new HashMap<Ref, Object>();
+
     private static GlobalContext instance = new GlobalContext();
 
     private GlobalContext() { }
@@ -17,6 +19,10 @@ public final class GlobalContext extends Context {
 
     @Override
     <T> T get(Ref<T> ref) {
-        return ref.content.value;
+        if (!inTxMap.containsKey(ref)) {
+            RefTuple<T, Long> tuple = ref.content;
+            inTxMap.put(ref, tuple.value);
+        }
+        return (T)inTxMap.get(ref);
     }
 }
